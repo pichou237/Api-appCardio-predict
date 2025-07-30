b7e14c29916d21bbe4a84143dfd00a195e2b35d6
pichou <calvinsteph237@gmail.com>
2025-07-30 14:12:20 +0200

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1795,7 +1795,7 @@ warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
- app = Flask(__name__)
+app = Flask(__name__)
 # CORS(app, resources={r"/*": {"origins": "*"}})
 # socketio = SocketIO(app, cors_allowed_origins="*")
 
